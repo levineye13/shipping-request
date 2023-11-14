8e78b08cc601b0db92ad91a15af63eb18d5fc092
Oleg Zharov <levineye@gmail.com>
2023-11-15 00:58:25 +0300

    feat: добавить компонент логотипа

---
+++ src/components/logo/index.tsx
@@ -0,0 +1,11 @@
+import React, { FC, ReactElement } from 'react';
+
+const Logo: FC = (): ReactElement => {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' width='31' height='31' fill='none'>
+      <circle cx='15.5' cy='15.5' r='15.5' fill='#fff' />
+    </svg>
+  );
+};
+
+export default Logo;
