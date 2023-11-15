8e3f02d7835573d3ccd02f465948c55b237d65c8
Oleg Zharov <levineye@gmail.com>
2023-11-15 13:42:31 +0300

    feat: закрыать бэекграунд при клике на него

--- src/components/background/index.tsx
+++ src/components/background/index.tsx
@@ -4,15 +4,21 @@ import styles from './index.module.scss';
 
 interface IBackground {
   readonly isActive: boolean;
+  readonly onClose: () => void;
   readonly className?: string;
 }
 
-const Background: FC<IBackground> = ({ isActive, className }): ReactElement => {
+const Background: FC<IBackground> = ({
+  isActive,
+  onClose,
+  className,
+}): ReactElement => {
   return (
     <div
       className={`${styles.background} ${
         isActive ? styles.background_active : ''
       } ${className || ''}`}
+      onClick={onClose}
     />
   );
 };
