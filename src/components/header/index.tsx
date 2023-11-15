99138856e93ecc29422897393e57de682a865a24
Oleg Zharov <levineye@gmail.com>
2023-11-15 13:46:06 +0300

    feat: пробросить хендлер закрытия сайдбара

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,14 +2,20 @@ import React, { FC, ReactElement } from 'react';
 
 import Logo from '../logo';
 import Menu from '../menu';
+import ButtonBurger from '../button-burger';
 import styles from './index.module.scss';
 
-const Header: FC = (): ReactElement => {
+interface IHeader {
+  readonly setSidebar: () => void;
+}
+
+const Header: FC<IHeader> = ({ setSidebar }): ReactElement => {
   return (
     <header className={styles.header}>
       <div className={styles.header__content}>
         <Logo />
         <Menu className={styles.header__menu} />
+        <ButtonBurger onClick={setSidebar} />
       </div>
     </header>
   );
