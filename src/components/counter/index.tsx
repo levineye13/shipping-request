95cf109537f611774fc56055815853ec1024dd0a
Oleg Zharov <levineye@gmail.com>
2023-11-16 21:12:12 +0300

    feat: сверстать компонент счетчика

---
+++ src/components/counter/index.tsx
@@ -0,0 +1,48 @@
+import React, { FC, ReactElement } from 'react';
+
+import styles from './index.module.scss';
+
+interface ICounter {
+  readonly title: string;
+  readonly name: string;
+  readonly value: number;
+  readonly onIncrement: () => void;
+  readonly onDecrement: () => void;
+  readonly signature?: string;
+  readonly className?: string;
+}
+
+const Counter: FC<ICounter> = ({
+  title,
+  name,
+  value,
+  onIncrement,
+  onDecrement,
+  signature,
+  className,
+}): ReactElement => {
+  return (
+    <label className={`${styles.counter} ${className || ''}`}>
+      {title}
+      <div className={styles.counter__custom}>
+        <button
+          className={`${styles.counter__button} ${styles.counter__button_left}`}
+          onClick={onDecrement}
+        >
+          -
+        </button>
+        <span className={styles.counter__value}>
+          {`${value} ${signature || ''}`}
+        </span>
+        <button
+          className={`${styles.counter__button}  ${styles.counter__button_right}`}
+          onClick={onIncrement}
+        >
+          +
+        </button>
+      </div>
+    </label>
+  );
+};
+
+export default Counter;
