d40fc7e022076fee9d0f3a7877c77da30acbb84b
Oleg Zharov <levineye@gmail.com>
2023-11-18 14:53:43 +0300

    feat: добавить в филдсет возможность его удаления

--- src/components/fieldset/index.tsx
+++ src/components/fieldset/index.tsx
@@ -1,11 +1,14 @@
 import React, { FC, ReactElement } from 'react';
 
+import Delete from '../../images/delete.svg';
 import styles from './index.module.scss';
 
 interface IFieldset {
   readonly children: ReactElement | ReactElement[];
   readonly name: string;
   readonly title: string;
+  readonly withDelete?: boolean;
+  readonly onDelete?: () => void;
   readonly className?: string;
 }
 
@@ -13,12 +16,32 @@ const Fieldset: FC<IFieldset> = ({
   children,
   name,
   title,
+  withDelete,
+  onDelete,
   className,
 }): ReactElement => {
+  const handleDelete = (): void => {
+    if (onDelete) {
+      onDelete();
+    }
+  };
+
   return (
     <fieldset className={`${styles.fieldset} ${className || ''}`} name={name}>
       <p className={styles.fieldset__title}>{title}</p>
       {children}
+      <button
+        className={`${styles.fieldset__delete} ${
+          withDelete ? styles.fieldset__delete_active : ''
+        }`}
+        onClick={handleDelete}
+      >
+        <img
+          className={styles.fieldset__img}
+          src={Delete}
+          alt='Кнопка удаления'
+        />
+      </button>
     </fieldset>
   );
 };
